1aa45b83dd75b9430bad5f3293f8346fbe93e72c
Fozilbek Imomov <fozilbekimomov@gmail.com>
2022-11-26 19:56:21 +0500

    Named and Qualifier added

---
+++ app/src/main/java/uz/example/dagger_2_android3/core/di/annotation/MovieDBAnnotation.kt
@@ -0,0 +1,1 @@
+package uz.example.dagger_2_android3.core.di.annotationimport java.lang.annotation.Documentedimport javax.inject.Qualifier@Qualifier@Documented@Retention(AnnotationRetention.RUNTIME)annotation class MovieDBAnnotation
--- app/src/main/java/uz/example/dagger_2_android3/core/di/module/network/NetworkModule.kt
+++ app/src/main/java/uz/example/dagger_2_android3/core/di/module/network/NetworkModule.kt
@@ -4,6 +4,9 @@ import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import uz.example.dagger_2_android3.core.di.annotation.MovieDBAnnotation
+import uz.example.dagger_2_android3.core.di.annotation.NewsApiAnnotation
+import javax.inject.Named
 import javax.inject.Singleton
 
 
@@ -12,9 +15,20 @@ object NetworkModule {
     
     @Provides
     @Singleton
+    @NewsApiAnnotation
     fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://newsapi.org/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    @MovieDBAnnotation
+    fun get1Retrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://www.themoviedb.org/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
